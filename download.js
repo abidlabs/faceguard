c1c0965b3d264c995fe9f9aba0490e62137ce4b6
Abubakar Abid <a12d@stanford.edu>
2020-03-31 08:24:12 -0700

    Update download.js

--- download.js
+++ download.js
@@ -1,6 +1,6 @@
 if(window.matchMedia("(max-width: 767px)").matches){
     // The viewport is less than 768 pixels wide
-    $('#heading').html("Karma is for Desktop")
+    $('#heading').html("FaceGuard is for Desktop")
     $('#subheading').html("...since it uses your webcam. But get a link sent to your email that you can check out later!")    
     $('#submit-button').html("Send Link")    
     $('#p1').css('display', 'none')
